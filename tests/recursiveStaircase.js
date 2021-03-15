1ba56d9117b5b7eb0a5d19239392ea5376e05c2f
can agriboz <>
2021-03-15 11:21:24 +0100

    Add: Recursive stair case

--- tests/recursiveStaircase.js
+++ tests/recursiveStaircase.js
@@ -3,4 +3,19 @@ const recursiveStaircase = (n) => {
   return recursiveStaircase(n - 1) + recursiveStaircase(n - 2);
 };
 
-module.exports = recursiveStaircase;
+
+const numWaysBottomUp = (n) => {
+  if (n === 0 || n === 1) return 1;
+  nums = n + 1;
+  nums[0] = 1;
+  nums[1] = 1;
+  let range = (start, end) => Array.from(Array(end + 1).keys()).slice(start);
+
+  for (var i of range(2, n)) {
+    nums[i] = nums[i - 1] + nums[i - 2]
+    return nums;
+  }
+  return nums[n];
+};
+
+module.exports = { recursiveStaircase, numWaysBottomUp };
--- tests/recursiveStaircase.test.js
+++ tests/recursiveStaircase.test.js
@@ -1,5 +1,9 @@
-const recursiveStaircase = require('./recursiveStaircase');
+const { recursiveStaircase, numWaysBottomUp } = require('./recursiveStaircase');
 
 test('should return number of ways to be 5', () => {
   expect(recursiveStaircase(4)).toBe(5);
+});
+
+test('should return number of ways to be 5', () => {
+  expect(numWaysBottomUp(4)).toBe(5);
 });
